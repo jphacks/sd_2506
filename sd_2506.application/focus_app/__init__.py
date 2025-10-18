0fe54c68b1b747613895da3b62b0997c7aac59cb
k023c0076 <k023c0076@g.neec.ac.jp>
2025-10-19 06:23:48 +0900

    6:00

--- sd_2506.application/focus_app/__init__.py
+++ sd_2506.application/focus_app/__init__.py
@@ -3,7 +3,6 @@ import secrets
 
 app = Flask(__name__)
 app.config.from_object('focus_app.config')
-app.secret_key = '83d2ea90c7b14f8d65e2c0931a7fbe41b2e9d6a173c5f048a7e03d1bfc92e8ad'
 
 # session
 app.secret_key = secrets.token_hex(16)
